62e05d6967752a0a9dacc2b1cec1a07c8197b22a
Saul Shanabrook <s.shanabrook@gmail.com>
2019-05-23 12:11:43 -0400

    mockup of explorer

--- dataregistry/src/explorer.tsx
+++ dataregistry/src/explorer.tsx
@@ -3,32 +3,92 @@
 | Distributed under the terms of the Modified BSD License.
 |----------------------------------------------------------------------------*/
 
-import { ReactWidget, UseSignal } from '@jupyterlab/apputils';
-import { Token } from '@phosphor/coreutils';
-import * as React from 'react';
-import { IDataRegistry } from './dataregistry';
-import { IActiveDataset } from './active';
-import { Widget } from '@phosphor/widgets';
-import { style, classes } from 'typestyle';
-import { Classes } from '@blueprintjs/core';
+import { ReactWidget } from "@jupyterlab/apputils";
+import { Token } from "@phosphor/coreutils";
+import * as React from "react";
+import { Widget } from "@phosphor/widgets";
+import { style, classes } from "typestyle";
+import { Classes } from "@blueprintjs/core";
+
+// type Data = { [key: string]: Array<string> };
+
+// const addedDB: Data = {
+//   "file://": ["load contents"],
+//   "postgres://localhost:5432/postgres": ["load tables", "snippet"]
+// };
+
+// const addedDBLoaded: Data = {
+//   "file://": ["load contents"],
+//   "file://notebooks/": ["load contents"],
+//   "file://notebooks/Analysis.ipynb#cells/0/outputs/0": ["datagrid", "voyager"],
+//   "file://notebooks/Analysis.ipynb#cells/3/outputs/0": ["datagrid", "voyager"],
+//   "file://tmp.csv": ["datagrid", "snippet"],
+//   "postgres://localhost:5432/postgres": ["load tables", "snippet"],
+//   "postgres://localhost:5432/postgres/artists": ["datagrid", "snippet"],
+//   "postgres://localhost:5432/postgres/exhibitions": ["datagrid", "snippet"],
+//   "postgres://localhost:5432/postgres/press": ["datagrid", "snippet"]
+// };
+
+// const reloadWithNotebookOpen: Data = {
+//   "file://": ["load contents"],
+//   "file://notebooks/Analysis.ipynb#cells/0/outputs/0": ["datagrid", "voyager"],
+//   "file://notebooks/Analysis.ipynb#cells/3/outputs/0": ["datagrid", "voyager"],
+//   "postgres://localhost:5432/postgres": ["load tables", "snippet"]
+// };
+
+type NestedData = { [key: string]: [Array<string>, NestedData] };
+
+// const addedDBNested: NestedData = {
+//   "file://": [["load contents"], {}],
+//   "postgres://localhost:5432/postgres": [["load tables", "snippet"], {}]
+// };
+
+const addedDBLoadedNested: NestedData = {
+  "file://": [
+    [],
+    {
+      "tmp.csv": [["datagrid", "snippet"], {}],
+      "notebooks/": [
+        [],
+        {
+          "Analysis.ipynb#cells/": [
+            [],
+            {
+              "0/outputs/0": [["datagrid", "voyager"], {}],
+              "10/outputs/0": [["datagrid", "voyager"], {}]
+            }
+          ]
+        }
+      ]
+    }
+  ],
+  "postgres://localhost:5432/postgres/": [
+    [],
+    {
+      artists: [["datagrid", "snippet"], {}],
+      exhibitions: [["datagrid", "snippet"], {}],
+      press: [["datagrid", "snippet"], {}]
+    }
+  ]
+};
 
 const buttonClassName = style({
-  color: '#2196F3',
+  color: "#2196F3",
   borderRadius: 2,
-  background: '#FFFFFF',
+  background: "#FFFFFF",
   fontSize: 10,
   borderWidth: 0,
   marginRight: 12, // 2 + 10 spacer between
-  padding: '2px 4px',
+  padding: "2px 4px",
   $nest: {
-    '&:active': {
-      background: '#BDBDBD'
+    "&:active": {
+      background: "#BDBDBD"
     },
-    '&:active:hover': {
-      background: '#BDBDBD'
+    "&:active:hover": {
+      background: "#BDBDBD"
     },
-    '&:hover': {
-      background: '#E0E0E0'
+    "&:hover": {
+      background: "#E0E0E0"
     }
   }
 });
@@ -41,91 +101,62 @@ function Button({ onClick, text }: { onClick: () => void; text: string }) {
   );
 }
 
-const datasetClassName = style({
-  borderBottom: '1px solid #E0E0E0',
-  color: '#333333',
-  padding: 4,
-  paddingRight: 12,
-  paddingLeft: 12,
-  borderLeftWidth: 8,
-  borderLeftColor: 'white',
-  borderLeftStyle: 'solid',
-  $nest: {
-    '&:hover': {
-      borderLeftColor: '#E0E0E0'
-    },
-    '&:active': {
-      borderLeftColor: '#BDBDBD'
-    },
-    '&:active:hover': {
-      borderLeftColor: '#BDBDBD'
-    }
-  }
-});
+// const datasetClassName = style({
+//   borderBottom: "1px solid #E0E0E0",
+//   color: "#333333",
+//   padding: 4,
+//   paddingRight: 12,
+//   paddingLeft: 12,
+//   borderLeftWidth: 8,
+//   borderLeftColor: "white",
+//   borderLeftStyle: "solid",
+//   $nest: {
+//     "&:hover": {
+//       borderLeftColor: "#E0E0E0"
+//     },
+//     "&:active": {
+//       borderLeftColor: "#BDBDBD"
+//     },
+//     "&:active:hover": {
+//       borderLeftColor: "#BDBDBD"
+//     }
+//   }
+// });
 
-const activeDatasetClassName = style({
-  borderLeftColor: 'var(--jp-brand-color1)',
-  $nest: {
-    '&:hover': {
-      borderLeftColor: 'var(--jp-brand-color1)'
-    },
-    '&:active': {
-      borderLeftColor: 'var(--jp-brand-color1)'
-    },
-    '&:active:hover': {
-      borderLeftColor: 'var(--jp-brand-color1)'
-    }
-  }
-});
+// const activeDatasetClassName = style({
+//   borderLeftColor: "var(--jp-brand-color1)",
+//   $nest: {
+//     "&:hover": {
+//       borderLeftColor: "var(--jp-brand-color1)"
+//     },
+//     "&:active": {
+//       borderLeftColor: "var(--jp-brand-color1)"
+//     },
+//     "&:active:hover": {
+//       borderLeftColor: "var(--jp-brand-color1)"
+//     }
+//   }
+// });
 
-function DatasetCompononent({
-  url,
-  dataRegistry,
-  active
-}: {
-  url: URL;
-  dataRegistry: IDataRegistry;
-  active: IActiveDataset;
-}) {
-  const classNames = [datasetClassName];
-  if (active.active !== null && active.active.toString() === url.toString()) {
-    classNames.push(activeDatasetClassName);
-  }
-  const viewers = [...dataRegistry.viewersForURL(url)];
-  console.log('Dataset', {
-    url,
-    mimeType: dataRegistry.data.mimeTypesForURL(url),
-    possible: dataRegistry.possibleMimeTypesForURL(url)
-  });
-  viewers.sort();
+function DatasetCompononent({ data }: { data: NestedData }) {
   return (
-    <div
-      className={classes(...classNames)}
-      onClick={() => (active.active = url)}
-    >
-      <h3
-        style={{
-          fontSize: 12,
-          fontWeight: 'unset',
-          margin: 'unset',
-          overflow: 'hidden',
-          textOverflow: 'ellipsis',
-          direction: 'rtl',
-          textAlign: 'left'
-        }}
-      >
-        {url.toString()}
-      </h3>
-      <span>
-        {viewers.map((label: string) => (
-          <Button
-            key={label}
-            onClick={() => dataRegistry.viewURL(url, label)}
-            text={label}
-          />
-        ))}
-      </span>
-    </div>
+    <ul style={{ paddingLeft: 20 }}>
+      {Object.keys(data).map(url => (
+        <li style={{ listStyleType: "none" }}>
+          <span>
+            {Object.keys(data[url][1]).length !== 0 ? "▼" : ""}
+            {url}
+          </span>
+          <br />
+          <span>
+            {data[url][0].map(view => (
+              <Button onClick={() => null} text={view} />
+            ))}
+          </span>
+          <DatasetCompononent data={data[url][1]} />
+        </li>
+      ))}
+    </ul>
   );
 }
 
@@ -144,14 +175,14 @@ function Heading({
         paddingLeft: 12,
         paddingRight: 12,
         fontSize: 14,
-        letterSpacing: '0.1em',
-        margin: 'unset',
-        color: '#333333',
-        display: 'flex',
-        justifyContent: 'space-between',
+        letterSpacing: "0.1em",
+        margin: "unset",
+        color: "#333333",
+        display: "flex",
+        justifyContent: "space-between",
         borderBottom:
-          'var(--jp-border-width) solid var(--jp-toolbar-border-color)',
-        boxShadow: 'var(--jp-toolbar-box-shadow)'
+          "var(--jp-border-width) solid var(--jp-toolbar-border-color)",
+        boxShadow: "var(--jp-toolbar-box-shadow)"
       }}
     >
       Datasets
@@ -167,76 +198,45 @@ function Heading({
   );
 }
 
-class DataExplorer extends React.Component<
-  {
-    dataRegistry: IDataRegistry;
-    active: IActiveDataset;
-  },
-  { search: string }
-> {
+class DataExplorer extends React.Component<{}, { search: string }> {
   state: { search: string } = {
-    search: ''
+    search: ""
   };
 
-  urls(): Array<URL> {
-    return [...this.props.dataRegistry.data.URLs].filter(
-      url => url.toString().indexOf(this.state.search) !== -1
-    );
-  }
   render() {
     return (
       <div
         style={{
-          background: '#FFFFFF',
-          color: '#000000',
-          fontFamily: 'Helvetica',
-          height: '100%',
-          display: 'flex',
-          flexDirection: 'column'
+          background: "#FFFFFF",
+          color: "#000000",
+          fontFamily: "Helvetica",
+          height: "100%",
+          display: "flex",
+          flexDirection: "column"
         }}
       >
         <Heading
           search={this.state.search}
           onSearch={(search: string) => this.setState({ search })}
         />
-        <div style={{ flex: 1, overflow: 'auto' }}>
-          <UseSignal signal={this.props.dataRegistry.data.datasetsChanged}>
-            {() => (
-              <UseSignal signal={this.props.active.signal}>
-                {() =>
-                  this.urls().map((url: URL) => (
-                    <DatasetCompononent
-                      key={url.toString()}
-                      url={url}
-                      dataRegistry={this.props.dataRegistry}
-                      active={this.props.active}
-                    />
-                  ))
-                }
-              </UseSignal>
-            )}
-          </UseSignal>
+        <div style={{ flex: 1, overflow: "auto" }}>
+          <DatasetCompononent data={addedDBLoadedNested} />
         </div>
       </div>
     );
   }
 }
 
-export function createDataExplorer(
-  dataRegistry: IDataRegistry,
-  active: IActiveDataset
-): IDataExplorer {
-  const widget = ReactWidget.create(
-    <DataExplorer dataRegistry={dataRegistry} active={active} />
-  );
-  widget.id = '@jupyterlab-dataRegistry/explorer';
-  widget.title.iconClass = 'jp-SpreadsheetIcon  jp-SideBar-tabIcon';
-  widget.title.caption = 'Data Explorer';
+export function createDataExplorer(): IDataExplorer {
+  const widget = ReactWidget.create(<DataExplorer />);
+  widget.id = "@jupyterlab-dataRegistry/explorer";
+  widget.title.iconClass = "jp-SpreadsheetIcon  jp-SideBar-tabIcon";
+  widget.title.caption = "Data Explorer";
   return widget;
 }
 /* tslint:disable */
 export const IDataExplorer = new Token<IDataExplorer>(
-  '@jupyterlab/dataRegistry:IDataExplorer'
+  "@jupyterlab/dataRegistry:IDataExplorer"
 );
 
 export interface IDataExplorer extends Widget {}

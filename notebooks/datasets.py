e4da4de436281eab1dd6544e11fb8dba40e255f4
Saul Shanabrook <s.shanabrook@gmail.com>
2019-07-26 14:12:52 -0400

    Show how output pandas works

--- notebooks/datasets.py
+++ notebooks/datasets.py
@@ -6,9 +6,3 @@ def output_url(url):
     IPython.display.publish_display_data(
         {"application/x.jupyter.relative-dataset-urls+json": [url]}
     )
-
-
-def output_pandas(dataframe):
-    path = "tmp.csv"
-    dataframe.to_csv(path, index=False)
-    output_url(path)

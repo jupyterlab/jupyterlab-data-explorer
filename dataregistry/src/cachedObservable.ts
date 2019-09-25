a32cc7b2eaff50b64621ca8e1a21800a92a623fc
Saul Shanabrook <s.shanabrook@gmail.com>
2019-09-25 12:41:50 -0400

    Rewrite cached observable to be more precise and correct

--- dataregistry/src/cachedObservable.ts
+++ dataregistry/src/cachedObservable.ts
@@ -1,24 +1,55 @@
-import { Observable, BehaviorSubject, Subscriber, Subscription } from "rxjs";
+import {
+  Observable,
+  BehaviorSubject,
+  Subscriber,
+  Subscription,
+  Observer
+} from "rxjs";
 
-export const NOT_SUBSCRIBED = Symbol("NOT_SUBSCRIBED");
-export const COMPLETE = Symbol("COMPLETE");
-export const NO_VALUE = Symbol("NO_VALUE");
-export const NOT_FINAL = Symbol("NOT_FINAL");
-
+/**
+ * Possible states our observables can be in.
+ */
+const enum State {
+  initial = "initial",
+  waitingFirst = "waiting first",
+  waitingNext = "waiting next",
+  waitingFirstSubs = "waiting first and subscribed",
+  waitingNextSubs = "waiting next and subscribed",
+  complete = "complete",
+  error = "error"
+}
 /**
  * We want to capture the last emitted value and whether the observable is finalized yet.
  */
-type ObservableState<T> = {
-  subscription: Subscription | typeof NOT_SUBSCRIBED;
-  value: T | typeof NO_VALUE;
-  final: { error: any } | typeof COMPLETE | typeof NOT_FINAL;
-};
-
-const INITIAL_STATE: ObservableState<any> = {
-  subscription: NOT_SUBSCRIBED,
-  value: NO_VALUE,
-  final: NOT_FINAL
-};
+type ObservableState<T> =
+  // Initial state before  anyone subscries
+  | { state: State.initial }
+  // Someone is subscribed and waiting for the first value
+  | {
+      state: State.waitingFirst;
+      subscribers: Set<Subscriber<T>>;
+    }
+  | {
+      state: State.waitingFirstSubs;
+      subscription: Subscription;
+      subscribers: Set<Subscriber<T>>;
+    }
+  // We have an initial value and we are waiting for the next
+  | {
+      state: State.waitingNext;
+      subscribers: Set<Subscriber<T>>;
+      value: T;
+    }
+  | {
+      state: State.waitingNextSubs;
+      subscription: Subscription;
+      subscribers: Set<Subscriber<T>>;
+      value: T;
+    }
+  // The backing subscription has finished and we have a final value
+  | { state: State.complete; value: T }
+  // The backing subscription last errored
+  | { state: State.error; error: any };
 
 /**
  * `CachedObservable` is a refcounted observable that maintains a maximum of one subscription to its source.
@@ -34,7 +65,9 @@ const INITIAL_STATE: ObservableState<any> = {
  *
  * New subscriber:
  * 1. If in state 1, then subscribe, move to state 2
- * 2. if in state 2 or 3, return last value if it exists and final state, if it exists
+ * 2. if in state 2, return last value
+ * 3. If in state 3 and ended in Done, return last value
+ * 4. If in state 3 and ended in error, move to state 2, to retry the computation
  *
  * Unsubscribe:
  * 1. Should never be in state one when unsubscribing
@@ -42,69 +75,166 @@ const INITIAL_STATE: ObservableState<any> = {
  */
 export class CachedObservable<T> extends Observable<T> {
   public static from<T>(from: Observable<T>): CachedObservable<T> {
-      if (from instanceof CachedObservable) {
-          return from
-      }
-      return new CachedObservable(from);
+    if (from instanceof CachedObservable) {
+      return from;
+    }
+    return new CachedObservable(from);
   }
   private constructor(from: Observable<T>) {
     super(subscriber => {
-      this._subscribers.add(subscriber);
       const state = this.state.value;
-      if (state.subscription === NOT_SUBSCRIBED && state.final === NOT_FINAL) {
-        this._setState({
-          subscription: from.subscribe(
-            v => {
-              this._setState({ value: v });
-              this._subscribers.forEach(s => s.next(v));
-            },
-            e => {
-              this._setState({ final: { error: e } }),
-                this._subscribers.forEach(s => s.error(e));
-            },
-            () => {
-              this._setState({ final: COMPLETE }),
-                this._subscribers.forEach(s => s.complete());
-            }
-          )
-        });
-      }
-      if (state.value !== NO_VALUE) {
-        subscriber.next(state.value);
-      }
-      if (state.final === COMPLETE) {
-        subscriber.complete();
-      } else if (state.final !== NOT_FINAL) {
-        subscriber.error(state.final.error);
-      }
-
-      return () => {
-        // delete subscriber on cleanup and unsubscribe from parent
-        // if there are no others left.
-        this._subscribers.delete(subscriber);
-        const currentState = this.state.value;
-        if (
-          this._subscribers.size === 0 &&
-          currentState.subscription !== NOT_SUBSCRIBED
-        ) {
-          currentState.subscription.unsubscribe();
-
-          // If we haven't finished yet, reset the value and subscription
-          if (currentState.final === NOT_FINAL) {
-            this._setState({ subscription: NOT_SUBSCRIBED, value: NO_VALUE });
+      switch (state.state) {
+        case State.error:
+        case State.initial:
+          const subscribers = new Set([subscriber]);
+          this.state.next({
+            state: State.waitingFirst,
+            subscribers
+          });
+          const subscription = from.subscribe(this.observer);
+          const newState = this.state.value;
+          if (newState.state === State.waitingFirst) {
+            this.state.next({
+              ...newState,
+              state: State.waitingFirstSubs,
+              subscription
+            });
+          } else if (newState.state === State.waitingNext) {
+            this.state.next({
+              ...newState,
+              state: State.waitingNextSubs,
+              subscription
+            });
           } else {
-            // Otherwise, just reset the subscription, keeping the the last value and final status
-            this._setState({ subscription: NOT_SUBSCRIBED });
+            throw new Error(
+              `Cannot be in state ${newState.state} after subscribing`
+            );
           }
+
+          break;
+        case State.waitingFirst:
+        case State.waitingFirstSubs:
+          state.subscribers.add(subscriber);
+          break;
+        case State.waitingNextSubs:
+        case State.waitingNext:
+          state.subscribers.add(subscriber);
+          subscriber.next(state.value);
+          break;
+        case State.complete:
+          subscriber.next(state.value);
+          return () => {};
+        default:
+          const _exhaustiveCheck: never = state;
+      }
+      return () => {
+        const state = this.state.value;
+        switch (state.state) {
+          case State.initial:
+            throw new Error(
+              "Should not be in initial state when unsubscribing"
+            );
+          case State.waitingFirst:
+            throw new Error(
+              "Should not be waiting for subscription and first when unsubscribing"
+            );
+          case State.waitingNext:
+            throw new Error(
+              "Should not be waiting for subscription and next when unsubscribing"
+            );
+          case State.waitingNextSubs:
+          case State.waitingFirstSubs:
+            state.subscribers.delete(subscriber);
+            if (state.subscribers.size === 0) {
+              state.subscription.unsubscribe();
+              this.state.next({ state: State.initial });
+            }
+            return;
+          case State.complete:
+          case State.error:
+            return;
+          default:
+            const _exhaustiveCheck: never = state;
         }
       };
     });
   }
 
-  private _setState(newState: Partial<ObservableState<T>>) {
-    this.state.next({ ...this.state.value, ...newState });
+  private get observer(): Observer<T> {
+    return {
+      next: value => {
+        const state = this.state.value;
+        switch (state.state) {
+          case State.initial:
+            throw new Error("Shouldn't be in initial state on next");
+          case State.waitingNext:
+          case State.waitingFirst:
+            this.state.next({
+              ...state,
+              state: State.waitingNext,
+              value
+            });
+            state.subscribers.forEach(s => s.next(value));
+            return;
+          case State.waitingNextSubs:
+          case State.waitingFirstSubs:
+            this.state.next({
+              ...state,
+              state: State.waitingNextSubs,
+              value
+            });
+            state.subscribers.forEach(s => s.next(value));
+            return;
+          case State.complete:
+            throw new Error("Shouldn't be in complete state on next");
+          case State.error:
+            throw new Error("Shouldn't be in error state on next");
+          default:
+            const _exhaustiveCheck: never = state;
+        }
+      },
+      error: error => {
+        const state = this.state.value;
+        switch (state.state) {
+          case State.initial:
+            throw new Error("Shouldn't be in initial state on error");
+          case State.waitingFirst:
+          case State.waitingFirstSubs:
+          case State.waitingNext:
+          case State.waitingNextSubs:
+            this.state.next({ state: State.error, error });
+            return;
+          case State.complete:
+            throw new Error("Shouldn't be in complete state on error");
+          case State.error:
+            throw new Error("Shouldn't be in error state on error");
+          default:
+            const _exhaustiveCheck: never = state;
+        }
+      },
+      complete: () => {
+        const state = this.state.value;
+        switch (state.state) {
+          case State.initial:
+            throw new Error("Shouldn't be in initial state on complete");
+          case State.waitingFirst:
+          case State.waitingFirstSubs:
+            throw new Error("Should have recieved value before complete");
+          case State.waitingNext:
+          case State.waitingNextSubs:
+            this.state.next({ state: State.complete, value: state.value });
+            return;
+          case State.complete:
+            throw new Error("Shouldn't be in complete state on complete");
+          case State.error:
+            throw new Error("Shouldn't be in error state on complete");
+          default:
+            const _exhaustiveCheck: never = state;
+        }
+      }
+    };
   }
-
-  public state = new BehaviorSubject<ObservableState<T>>(INITIAL_STATE);
-  private _subscribers: Set<Subscriber<T>> = new Set();
+  public state = new BehaviorSubject<ObservableState<T>>({
+    state: State.initial
+  });
 }

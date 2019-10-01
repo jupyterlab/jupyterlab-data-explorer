a8105817c876ee224a6c68135401ccf9ee261553
kgryte <kgryte@gmail.com>
2019-09-30 19:32:04 -0700

    Add meta package

---
+++ packages/dataregistry-metapackage/src/index.ts
@@ -0,0 +1,12 @@
+/**
+ * @license BSD-3-Clause
+ *
+ * Copyright (c) 2019 Project Jupyter Contributors.
+ * Distributed under the terms of the 3-Clause BSD License.
+ */
+
+// Keep in alphabetical order...
+import '@jupyterlab/dataregistry';
+import '@jupyterlab/dataregistry-csvviewer';
+import '@jupyterlab/dataregistry-extension';
+import '@jupyterlab/dataregistry-registry';

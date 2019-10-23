81c9baf2e105cf354c0af60e24d859ec3fb77f81
kgryte <kgryte@gmail.com>
2019-10-23 13:18:09 -0700

    Setup linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,31 +6,31 @@
  */
 
 module.exports = {
-  "env": {
-    "browser": true,
-    "es6": true
-  },
-  "extends": [
-    "plugin:@typescript-eslint/eslint-recommended",
-    "prettier",
-    "prettier/@typescript-eslint"
-  ],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "project": "tsconfig.json",
-    "ecmaFeatures": {
-      "jsx": true
-    },
-    "ecmaVersion": 2018,
-    "sourceType": "module"
-  },
-  "plugins": [
-    "react",
-    "react-hooks",
-    "@typescript-eslint"
-  ],
-  "rules": {
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn"
-  }
+	env: {
+		browser: true,
+		es6: true
+	},
+	extends: [
+		'plugin:@typescript-eslint/eslint-recommended',
+		'prettier',
+		'prettier/@typescript-eslint'
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: 'tsconfig.json',
+		ecmaFeatures: {
+			jsx: true
+		},
+		ecmaVersion: 2018,
+		sourceType: 'module'
+	},
+	plugins: ['prettier', 'react', 'react-hooks', '@typescript-eslint'],
+	rules: {
+		'react-hooks/rules-of-hooks': 'error',
+		'react-hooks/exhaustive-deps': 'warn',
+		'prettier/prettier': ['error', { singleQuote: true }],
+		indent: ['error', 2],
+		'linebreak-style': ['error', 'unix'],
+		'no-console': ['error', { allow: ['warn', 'error'] }]
+	}
 };

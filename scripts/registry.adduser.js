9757c1cb831767e645afe44dcdfdfb380b706c22
kgryte <kgryte@gmail.com>
2019-10-30 20:21:35 -0700

    Add script to registry a user with the local registry

---
+++ scripts/registry.adduser.js
@@ -0,0 +1,32 @@
+/**
+ * @license BSD-3-Clause
+ *
+ * Copyright (c) 2019 Project Jupyter Contributors.
+ * Distributed under the terms of the 3-Clause BSD License.
+ */
+
+'use strict';
+
+var RegistryClient = require('npm-registry-client');
+
+var client = new RegistryClient();
+var params = {
+  timeout: 1000,
+  auth: {
+    username: 'foo',
+    password: 'bar',
+    email: 'foo@bar.com'
+  }
+};
+
+client.adduser('http://localhost:4873', params, done);
+
+function done(error, data, raw, res) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  console.log('Login succeeded');
+  console.log('data: %s', JSON.stringify(data, null, 2));
+  console.log('NPM access token: %s', data.token);
+}
